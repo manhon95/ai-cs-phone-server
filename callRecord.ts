f7a84092c9d6f9c2feb9b2424d2d2cc58ac060b9
VIctor Law <manhon95@gmail.com>
2023-06-30 21:35:07 +0800

    [front end] delete

--- callRecord.ts
+++
@@ -1,8 +0,0 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Set environment variables for your credentials
-// Read more at http://twil.io/secure
-const accountSid = "AC5ebcbf56bb7d8db152f8338b1f271293";
-const authToken = "63b93b488742cab113a542bead3d725e";
-const client = require("twilio")(accountSid, authToken);
-
-client.calls.each((call: any) => console.log(call));
